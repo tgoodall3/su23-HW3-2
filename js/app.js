a281c88acf6f8a954c870d918e87795282471242
tgoodall3 <Tylergoodall3@gmail.com>
2023-07-07 21:22:35 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,8 +7,10 @@ function setup(){
 function draw(){
     background(220)
     var diameter = width;
+    // set diameter to width of canvas
 
     while (diameter >= 10){
+
         circle(width/2, height/2, diameter);
         diameter-=10;
     }
